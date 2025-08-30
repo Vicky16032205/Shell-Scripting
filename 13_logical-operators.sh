470e600b2d89e602ee295e1276ed3f77e6cbafe3
Vicky16032205 <vickyguptagkp55@gmail.com>
2025-08-31 01:01:37 +0530

    added logical operators

--- 13_logical-operators.sh
+++ 13_logical-operators.sh
@@ -13,8 +13,29 @@ else
 fi
 
 
+
+read -p "Do you have Driving License : " dl
+read -p "Enter your age: " ages
+
+age=${ages,,}
+
+if [[ $age -ge 18 ]] && [[ $dl == "yes" ]]
+then
+        echo "You can drive"
+elif [[ $age -ge 18 ]] || [[ $dl == "no" ]]
+then
+        echo "You are required to make driving license"
+
+else
+        echo "You cannot drive"
+fi
+
+
 output
 
-Enter your age: 21
-Enter your country name : INDIA
+Enter your age: 18
+Enter your country name : india
 You can vote
+Do you have Driving License : no
+Enter your age: 21
+You are required to make driving license
