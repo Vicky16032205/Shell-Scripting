fd72134bc40adc49561737aaf95227cf8cfa5017
Vicky16032205 <vickyguptagkp55@gmail.com>
2025-08-31 20:26:55 +0530

    Learning for loops in shell scripting

---
+++ 14_loops.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+        echo "Iterating over for loop having index $i "
+done
+
+
+
+for name in Ram Shyam Ghanshyam BabaRao Laxman
+do
+        echo "Name is $name"
+done
+
+
+OUTPUT:
+
+Iterating over for loop having index 1
+Iterating over for loop having index 2
+Iterating over for loop having index 3
+Iterating over for loop having index 4
+Iterating over for loop having index 5
+Iterating over for loop having index 6
+Iterating over for loop having index 7
+Iterating over for loop having index 8
+Iterating over for loop having index 9
+Iterating over for loop having index 10
+Name is Ram
+Name is Shyam
+Name is Ghanshyam
+Name is BabaRao
+Name is Laxman
