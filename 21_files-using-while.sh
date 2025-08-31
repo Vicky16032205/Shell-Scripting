6b1432675ba234fe7a7dc49cd544dac0f935a7f4
Vicky16032205 <vickyguptagkp55@gmail.com>
2025-08-31 23:31:39 +0530

    files reading using while loops

---
+++ 21_files-using-while.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+while read myVar
+do
+        echo "Value from file is $myVar"
+done < names.txt
+
+
+
+OUTPUT:
+
+
+
+Value from file is vicky
+Value from file is ankit
+Value from file is aman
+Value from file is ayush
+Value from file is aditya
+Value from file is virat
+Value from file is ram
+Value from file is shyam
+Value from file is ghanshyam
+Value from file is yajman
